0dfb2d883d9923d0e145c1f409b11e8df5d841c7
luechtdiode <roland.seidel@interpolar.ch>
2016-08-28 23:52:05 +0200

    cleanup in package & bower.json

--- mobile/setup.cmd
+++ mobile/setup.cmd
@@ -1,10 +1,11 @@
 # npm install -g cordova ionic
 # bower install ngCordova --save
 # ionic start GymCompetition sidemenu
-# ionic platform android
-# ionic resources
-# ionic build android
+ionic platform android
+ionic resources
+ionic build android
 # publishing:
-# cordova plugin rm cordova-plugin-console
-# cordova build --release android
-ionic server --lab
+cordova plugin rm cordova-plugin-console
+cordova build
+cordova build --release android
+# ionic serve --lab
