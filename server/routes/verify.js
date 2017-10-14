46b37e29eb1dc18476c21d16cec0834bc0168f3e
Roland Seidel <register@interpolar.ch>
2017-10-14 18:03:56 +0200

    clean package.json, fix verify.materialize

--- server/routes/verify.js
+++ server/routes/verify.js
@@ -49,10 +49,7 @@ exports.verifyAdmin = (req, res, next) => {
 
 exports.materializeUser = (callback) => (req, res, next) => {
     if (req.decoded && req.decoded.id) {
-        User.findById(req.decoded.id, (err, user, info) => {
-            callback(err, user, info);
-            next();
-        });
+        User.findById(req.decoded.id,callback);
     }
     else {
         var err = new Error('You are not authorized to perform this operation!');
